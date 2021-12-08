ee5993c2211c2a02a25f591b66f3f889b2140125
harsha <harsha00032@gmail.com>
2021-12-08 15:59:14 +0530

    first commit

---
+++ plural/fundmentals/ng-fundamentals/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+
+import {
+  EventThumbnailComponent,
+  EventsListComponent,
+  CreateEventComponent,
+  EventListResolverService,
+  NavbarComponent,
+  EventService,
+  EventDetailsComponent,
+  EventRouterActivatorService
+} from './events/index'
+
+import { Error404Component } from './errors/404.component';
+import { EventsAppComponent } from './events-app.component'
+
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
+  ],
+
+  providers: [EventService,
+     EventRouterActivatorService,
+     EventListResolverService,
+     {
+       provide: 'canDeactivateCreateEvent',
+       useValue: checkDirtyState
+     }
+    ],
+  bootstrap: [EventsAppComponent]
+})
+export class EventsAppModule { }
+
+export function checkDirtyState(component: CreateEventComponent){
+  if (component.isDirty)
+   return window.confirm('You have not saved this event, do you really want to cancel?')
+ return true;
+}
